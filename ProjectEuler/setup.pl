1975c3a42eaea78f947a543e637474fd3f85c9c1
starter <waidsearch@gmail.com>
2017-12-31 16:19:23 +0100

    Added main.cpp in setup and added c++ compilation flags

--- ProjectEuler/setup.pl
+++ ProjectEuler/setup.pl
@@ -2,20 +2,48 @@
 
 use warnings;
 use strict;
-use File::Copy;
+use File::Copy; 
+use File::Basename;
 
-my $sources = "sources";
-my $build_file = "templates/CMakeLists.txt";
+# Function definitions
 
-opendir( my $dirs, $sources );
+sub CopyFile 
+{
+	my ($file, $todir) = @_;
 
-while ( my $dir = readdir $dirs ) {
-	my $todir = $sources . '/' . $dir;
+	print "[Copy] $file to $todir... ";
+	copy($file, $todir) or die "[Failed]\n Error: $!";
+	print "[done]\n";
+}
+
+sub CopyFileIfNotPresent
+{
+	my ($file, $todir) = @_;
+	my $basename = basename($file);
+	
+	unless (-f "$todir/$basename") {
+		CopyFile($file, $todir);
+	}
+}
+
+# Main
+
+my $sources_folder = "sources";
+my $templates_folder = "templates";
+
+my $build_file = "CMakeLists.txt";
+my $main_file = "main.cpp";
+
+opendir( my $dirs, $sources_folder );
+
+while ( my $dir = readdir $dirs ) 
+{
+	my $todir = $sources_folder . '/' . $dir;
     next unless -d $todir;
     next if $dir eq '.' or $dir eq '..';
 	
-	print("[Copy] $build_file to $todir");
-	copy($build_file, $todir) or die "[Failed] Copy: $!";
+	CopyFile("$templates_folder/$build_file", $todir);
+	CopyFileIfNotPresent("$templates_folder/$main_file", $todir);
 }
 
 closedir $dirs;
