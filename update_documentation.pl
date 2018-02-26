d46f01214ae4b1726da83b1c736faa5e4fcd51b8
starter <waidsearch@gmail.com>
2018-02-26 22:24:44 +0100

    Updated the documents to prepare for the katas integration

--- update_documentation.pl
+++ update_documentation.pl
@@ -3,17 +3,101 @@
 use warnings;
 use strict;
 
-# Function definitions
+# Global functions
+
+sub ReadLinesFromFile
+{
+	my ($filename) = @_;
+	
+	open my $in, '<', $filename or die "Could not open file '$filename' $!";
+	chomp(my @lines = <$in>);
+	close $in;
+	
+	return @lines;
+}
+
+sub WriteContentToFile
+{
+	my ($filename, $filecontent) = @_;
+	
+	open my $out, '>', $filename or die "Can't write to file '$filename': $!";
+	print $out $filecontent;
+	close $out;
+}
+
+sub ListSubDirectories
+{
+	my ($dir) = @_;
+	opendir ( my $dh, $dir );
+	return grep {-d "$dir/$_" && ! /^\.{1,2}$/} readdir($dh);
+}
+
+sub FindNextEmptyLine
+{
+	my ($start, @lines) = @_;
+	
+	foreach my $i ($start .. $#lines) 
+	{
+		if ($lines[$i] eq "") {
+			return $i;
+		}
+	}
+	
+	return $#lines;
+}
+
+# Katas related functions
+
+sub RetrieveLanguages
+{
+	my ($dir) = @_;
+	my @subdirs = ListSubDirectories($dir);
+	
+	foreach my $subdir (@subdirs) {
+		$subdir = "[$subdir]($dir/$subdir)";
+	}
+	
+	return @subdirs;
+}
+
+sub BuildKataLink
+{
+	my ($dir, $name) = @_;
+	return "* [$name]($dir/README.md) : implemented in " . join(', ', RetrieveLanguages($dir)) . "\n";
+}
+
+sub BuildKataLinkForDir
+{
+	my ($dir) = @_;
+    my @readme = ReadLinesFromFile("$dir/README.md");
+	my $name = substr $readme[2], 1;
+	
+	$name =~ s/^\s+|\s+$//g;
+	
+	return BuildKataLink($dir, $name);
+}
+
+sub BuildKatasLinks
+{
+	my $sources_folder = "Kata/sources";
+	my $links = "";
+	my @dirs = ListSubDirectories($sources_folder);
+
+	foreach my $dir (@dirs) {
+		$links .= BuildKataLinkForDir("$sources_folder/$dir");
+	}
+	
+	return $links;
+}
+
+# Project Euler related functions
 
 sub RetrieveProjectEulerInfo
 {
 	my ($dir) = @_;
-	my $file = $dir . "/main.cpp";
-	
-	open my $fh, $file or die "Could not open '$file' $!\n";
-	my @data = <$fh>;
+
+	my @data = ReadLinesFromFile($dir . "/main.cpp");
     my @info = split(/Project Euler Problem | - |\n/, $data[1]);
-	close $fh;
 	
 	return @info;
 }
@@ -41,77 +125,42 @@ sub BuildProjectEulerLinks
 {
 	my $sources_folder = "ProjectEuler/sources";
 	my $links = "";
+	my @dirs = ListSubDirectories($sources_folder);
 
-	opendir ( my $dirs, $sources_folder );
-
-	while ( my $dir = readdir $dirs ) 
-	{
-		next if $dir eq '.' or $dir eq '..';
-		
-		my $todir = "$sources_folder/$dir";
-		next unless -d $todir;
-		
-		$links .= BuildProjectEulerLinkForDir($todir);
+	foreach my $dir (@dirs) {
+		$links .= BuildProjectEulerLinkForDir("$sources_folder/$dir");
 	}
-
-	closedir $dirs;
 	
 	return $links;
 }
 
-sub FindNextEmptyLine
-{
-	my ($start, @lines) = @_;
-	
-	foreach my $i ($start .. $#lines) 
-	{
-		if ($lines[$i] eq "") {
-			return $i - $start;
-		}
-	}
-	
-	return $#lines - $start;
-}
-
-sub ReadLinesFromFile
-{
-	my ($filename) = @_;
-	
-	open my $in, '<', $filename or die "Could not open file '$filename' $!";
-	chomp(my @lines = <$in>);
-	close $in;
-	
-	return @lines;
-}
+# Main
 
 sub UpdateFileContent
 {
 	my (@lines) = @_;
 	my $filecontent = "";
-	my $add = 0;
+	my $i = 0;
 	
-	foreach my $i (0 .. $#lines) 
-	{
-		last if $i + $add >= $#lines;
+	while ($i < $#lines)
+	{		
+		$filecontent .= $lines[$i] . "\n";
 		
-		$filecontent .= $lines[$i + $add] . "\n";
-		if ($lines[$i + $add] eq "### Problems") 
+		if ($lines[$i] eq "### Katas") 
 		{
-			$add = FindNextEmptyLine($i+2, @lines) + 1;
-			$filecontent .= "\n" . BuildProjectEulerLinks() . "\n";
+			$i = FindNextEmptyLine($i+2, @lines) - 1;
+			$filecontent .= "\n" . BuildKatasLinks();
 		}
+		if ($lines[$i] eq "### Problems") 
+		{
+			$i = FindNextEmptyLine($i+2, @lines) - 1;
+			$filecontent .= "\n" . BuildProjectEulerLinks();
+		}
+		
+		$i++;
 	}
 	
 	return $filecontent;
-}
-
-sub WriteContentToFile
-{
-	my ($filename, $filecontent) = @_;
-	
-	open my $out, '>', $filename or die "Can't write to file '$filename': $!";
-	print $out $filecontent;
-	close $out;
 }
 
 sub UpdateReadmeFile
@@ -123,7 +172,6 @@ sub UpdateReadmeFile
 	WriteContentToFile($filename, $filecontent);
 }
 
-# Main
 UpdateReadmeFile();	
 	
 
